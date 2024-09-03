10bed3ee625a5c53fea48c81feaee0922f6d3082
vijithaopatha7 <2021cs205@stu.ucsc.cmb.ac.lk>
2024-09-04 03:11:49 +0530

    Tutorial 10

---
+++ Tutorial10/Q3.scala
@@ -0,0 +1,37 @@
+object Q3{
+  def main(args: Array[String]): Unit = {
+    val account1 = new Account("John", 1000)
+    val account2 = new Account("Bob", 1500)
+
+    println(account1)
+    println(account2)
+
+    account1.transfer(account2, 300)
+
+    println(account1)
+    println(account2)
+  }
+}
+
+class Account(val accountHolder: String, var balance: Double) {
+  def deposit(amount: Double): Unit = {
+    require(amount > 0)
+    balance += amount
+  }
+
+  def withdraw(amount: Double): Unit = {
+    require(amount > 0)
+    require(balance >= amount)
+    balance -= amount
+  }
+
+  def transfer(targetAccount: Account, amount: Double): Unit = {
+    require(amount > 0)
+    require(balance >= amount)
+
+    withdraw(amount)
+    targetAccount.deposit(amount)
+  }
+
+  override def toString: String = s"Account Holder: $accountHolder, Balance: $balance"
+}
